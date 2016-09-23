86a977e55072cc560e6c929d794c8f68ced0e819
Peter Andersson <fshsweden@hotmail.com>
2016-09-23 18:30:12 +0200

    First checkin

---
+++ HelloJenkins/src/Hello.java
@@ -0,0 +1,13 @@
+
+public class Hello {
+
+	public Hello() {
+		System.out.println("Hello constructor!");
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("main()");
+		Hello h = new Hello();
+	}
+
+}
